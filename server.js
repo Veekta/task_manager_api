e8d36d2eb93e35d3e0622560c371a8c865f02018
veekta <vsunday.1710@gmail.com>
2023-06-30 17:46:09 +0100

    fix bug

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("express")();
-const server = require("http").createServer(app);
+const serve = require("node:http").createServer(app);
 require("./config/db");
 const MouseMovement = require("./models/trackUser");
 
 // Initialize Socket.IO
-const io = require("socket.io")(server);
+const io = require("socket.io")(serve);
 
 const port = process.env.PORT || 5000;
 const UserRouter = require("./router/user");
