4a723183b6753f722823abab0bbaa08395315784
veekta <vsunday.1710@gmail.com>
2023-06-30 17:34:15 +0100

    fix bug

--- models/trackUser.js
+++ models/trackUser.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const mouseMovementSchema = new mongoose.Schema({
   user: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
   x: Number,
